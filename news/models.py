88b4bd09664dedcab4e4a287527ffdd5ba727d74
lfborjas <luis.borjas@escolarea.com>
2011-05-03 12:30:59 -0600

    creados modelos

--- news/models.py
+++ news/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Submission(models.Model):
+    GRAVITY = 1.8
+
+    title = models.CharField(max_length=240)
+
+    url = models.URLField(verify_exists=True, blank=True, default="")
+    text = models.TextField(blank=True, default="")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    upvotes = models.IntegerField(default=1)
+
+    def points(self):
+        from datetime import datetime, timedelta
+        from future import __division__
+        age = (datetime.now() - self.created_at).seconds / 3600
+        return (self.upvotes-1) / (age+2)**Entry.GRAVITY
+    
+    def __unicode__(self):
+        return self.title
+    
+    @models.permalink
+    def get_absolute_url(self):
+        pass
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    last_modified = models.DateTimeField(auto_now = True)
+    submission = models.ForeignKey(Submission, null=True, blank=True, related_name='comments')
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='replies')
+
+    @models.permalink
+    def get_absolute_url(self):
+        pass
+
+    def __unicode__(self):
+        return self.text
+
